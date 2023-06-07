9ddfb5a2250f9846ff11b62fbe4e5346df8a73f9
BuntinMA_Home <BuntinMA@mail.ru>
2023-06-07 20:20:47 +0300

    Initial commit.

---
+++ src/main/java/org/example/Event.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.time.DateTimeException;
+
+// событие
+public class Event {
+    private DateTimeException datetime; // Дата и время начала события
+    private Long length; //длительность события
+    private Place place; // место проведения
+    private int guests; // количество посетителей
+    private int master; // ответственное лицо
+    private PersonList personList; //список задействованных сотрудников
+
+
+
+}
---
+++ src/main/java/org/example/PersonList.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class PersonList { // лист задействованный сотрудников в формате словаря: сотрудник-задача.
+
+    private ArrayList(Person) {
+
+    }
+}
