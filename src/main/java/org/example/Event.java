f4a64fbe756e99ba07056c0897969c1266d27383
BuntinMA_Home <BuntinMA@mail.ru>
2023-08-30 16:20:42 +0300

    Работа с БД. Подготовка запросов для создания таблиц.

--- src/main/java/org/example/Event.java
+++ src/main/java/org/example/Event.java
@@ -13,15 +13,37 @@ public class Event {
 }
 
 //CREATE TABLE Events (Id SERIAL PRIMARY KEY,
-// EventTypeId INTEGER REFERENCES EventType (Id),
-// Name CHARACTER VARYING(100),
-// PlaceId INTEGER REFERENCES Places (Id),
-// Date TIMESTAMP,
-// Lenght INTERVAL,
-// GuestNumber (Integer),
-// MasterId INTEGER REFERENCES Users (Id),
+// EventTypeId INTEGER REFERENCES EventTypes (Id),
+// Name CHARACTER VARYING(100) NOT NULL,
+// PlaceId INTEGER REFERENCES Places (Id) NOT NULL,
+// Date TIMESTAMP NOT NULL,
+// Lenght INTERVAL NOT NULL,
+// GuestNumber Integer,
+// MasterId INTEGER REFERENCES Users (Id) NOT NULL,
 // PersonList,
 // );
 
+//CREATE TABLE Users (Id SERIAL PRIMARY KEY,
+// Name CHARACTER VARYING(100) NOT NULL,
+// JobTitle INTEGER REFERENCES JobTitles (Id) NOT NULL,
+// -- UserFunctions INTEGER REFERENCES UserFunctions (Id),
+// );
+
+//CREATE TABLE EventTypes (Id SERIAL PRIMARY KEY,
+// EventType CHARACTER VARYING(60) NOT NULL,
+// );
+
+//CREATE TABLE Places (Id SERIAL PRIMARY KEY,
+// Place CHARACTER VARYING(30) NOT NULL,
+// Volume Integer,
+// );
+
+//CREATE TABLE JobTitles (Id SERIAL PRIMARY KEY,
+// JobTitle CHARACTER VARYING(60) NOT NULL,
+// );
+
+//CREATE TABLE UserFunctions (Id SERIAL PRIMARY KEY,
+// UserFunction CHARACTER VARYING(60) NOT NULL,
+// );
 
 }
